33f925d18051895729c05ea3ad40bf23bc6974e3
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2025-03-13 22:13:14 -0700

    change CardManager to map over source_pod_id, Cards map over categories, add shadCN recommended Typography styles

--- src/components/CardManager.tsx
+++ src/components/CardManager.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
 import axios from "axios";
 import CategoryCard from "./CategoryCard";
-import { compatComponent } from "../types/types.ts";
+import { CompatComponent, GroupedCompatData } from "../types/types.ts";
 
 interface CardManagerProps {
   selectedCode: string;
 }
 
-type GroupedCompatData = Record<string, compatComponent[]>;
-
 export default function CardManager({ selectedCode }: CardManagerProps) {
-  const [compatData, setCompatData] = React.useState<compatComponent[]>([]);
+  const [compatData, setCompatData] = React.useState<CompatComponent[]>([]);
+  const [error, setError] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     if (selectedCode === "") {
@@ -18,39 +17,47 @@ export default function CardManager({ selectedCode }: CardManagerProps) {
     }
 
     async function fetchCompatData() {
-      const response = await axios.get(
-        `http://localhost:8080/api/compatibility?code=${selectedCode}`,
-      );
-      setCompatData(response.data);
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/compatibility?code=${selectedCode}`,
+        );
+        setCompatData(response.data);
+        setError(null);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to fetch compatiblity data.");
+      }
     }
 
     fetchCompatData();
   }, [selectedCode]);
 
   if (selectedCode === "") {
-    return <div> please select a component</div>;
+    return <div>select a component to find compatible components</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
   }
 
   const groupedCompatData = compatData.reduce(
     (acc: GroupedCompatData, compatComponent) => {
-      if (!acc[compatComponent.category]) {
-        acc[compatComponent.category] = [];
+      if (!acc[compatComponent.source_pod_id]) {
+        acc[compatComponent.source_pod_id] = [];
       }
-      acc[compatComponent.category].push(compatComponent);
+      acc[compatComponent.source_pod_id].push(compatComponent);
       return acc;
     },
     {},
   );
 
-  console.log("groupedCompatData", groupedCompatData);
-
   return (
     <div className="card-manager">
-      {Object.keys(groupedCompatData).map((category) => (
+      {Object.keys(groupedCompatData).map((source_pod_id) => (
         <CategoryCard
-          key={category}
-          category={category}
-          compatData={groupedCompatData[category]}
+          key={source_pod_id}
+          source_pod_id={Number(source_pod_id)}
+          compatData={groupedCompatData[source_pod_id]}
         />
       ))}
     </div>
