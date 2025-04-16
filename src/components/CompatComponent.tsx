a2ddbfe4d4f9d7041847386786be804c05f8211b
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2025-04-16 01:47:42 -0700

    progress on compatComponent implementing expanded status with appropriate data and styles

--- src/components/CompatComponent.tsx
+++ src/components/CompatComponent.tsx
@@ -1,7 +1,8 @@
+import * as React from "react";
 import { CompatComponentType } from "@/types/types.ts";
 import { Toggle } from "@/components/ui/toggle";
 import { useSelectedCodesDispatch } from "@/lib/selectedCodeUtils";
-import { SquareArrowOutUpRight, Maximize2 } from "lucide-react";
+import { SquareArrowOutUpRight, Maximize, Minimize } from "lucide-react";
 
 interface CompatComponentProps {
   compCompData: CompatComponentType;
@@ -13,12 +14,11 @@ export function CompatComponent({
   isPressed,
 }: CompatComponentProps) {
   const dispatch = useSelectedCodesDispatch();
+  const [isExpanded, setIsExpanded] = React.useState(false);
 
   // NOTE: this compCompData category is pulled from the compatData query: CompatComponentType
   // whereas selectedCodes is pulled from the allCodes query: CodeObjType
   // the SQL query on the backend ensure they will be the sam, as the categories are pulled from the same table.
-
-  // const hasLink = compCompData.link;
 
   function handleToggle() {
     if (isPressed) {
@@ -35,16 +35,63 @@ export function CompatComponent({
   }
 
   return (
-    <div className="compat_component flex items-center gap-4 py-2">
+    <div className="compat_component flex items-start gap-2 py-2">
       <Toggle
         pressed={isPressed}
         onPressedChange={handleToggle}
-        className="data-[state=on]:bg-primary data-[state=on]:text-primary-foreground bg-secondary m-0 flex w-full justify-between rounded-sm border border-dotted p-0 px-4"
+        className="data-[state=on]:bg-primary data-[state=on]:text-primary-foreground bg-secondary data-[state=on]:hover:bg-primary/80 flex h-auto min-h-0 w-full items-start justify-between rounded-sm border border-dotted px-3 py-2.5"
       >
-        <small className="m-0 p-0">{compCompData.code}</small>
-        <p className="text-[10px] leading-none">{compCompData.note}</p>
+        {isExpanded ? (
+          <>
+            <div className="flex flex-1 flex-col items-start gap-0 text-start text-wrap">
+              <small className="f m-0 border-b-1 pb-1">
+                {compCompData.code}
+              </small>
+              <p className="mt-2">{compCompData.note}</p>
+              <p className="mt-2">
+                {compCompData.warning ? "# " + compCompData.warning : null}
+              </p>
+              <div></div>
+            </div>
+            <div className="flex flex-col items-end justify-end">
+              <p>{compCompData.status}</p>
+            </div>
+          </>
+        ) : (
+          <>
+            <small className="m-0 p-0">{compCompData.code}</small>
+
+            <p className="">{compCompData.note}</p>
+          </>
+        )}
       </Toggle>
-      <Maximize2 className="w-4" />
+      <div className="flex flex-col gap-2">
+        {isExpanded ? (
+          <>
+            <button className="p-1" onClick={() => setIsExpanded(!isExpanded)}>
+              <Minimize className="h-4.5 w-4.5" />
+            </button>
+            {compCompData.link ? (
+              <a
+                target="_blank"
+                href={compCompData.link}
+                rel="noreferrer"
+                className="p-1"
+              >
+                <SquareArrowOutUpRight className="h-4.5 w-4.5" />
+              </a>
+            ) : (
+              <div className="p-1">
+                <SquareArrowOutUpRight className="text-muted h-4.5 w-4.5" />
+              </div>
+            )}
+          </>
+        ) : (
+          <button className="p-1" onClick={() => setIsExpanded(!isExpanded)}>
+            <Maximize className="h-4.5 w-4.5" />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
