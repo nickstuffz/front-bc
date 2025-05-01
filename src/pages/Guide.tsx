0cb0e952fd4ac4d4bc0450d11ebffc8fc4d43c04
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2025-04-30 18:33:29 -0700

    Complete intitial Guide page content

--- src/pages/Guide.tsx
+++ src/pages/Guide.tsx
@@ -2,61 +2,72 @@ import { Badge } from "@/components/ui/badge";
 
 export function Guide() {
   return (
-    <div className="content flex flex-1 flex-col gap-4 p-2">
-      <h2 className="mb-2 pl-1">Component Codes</h2>
+    <div className="content mb-4 flex flex-1 flex-col gap-4 p-2">
+      <h2 className="pb-3 pl-1">Component Codes</h2>
       <div className="flex flex-col gap-4">
-        <div className="flex flex-col">
-          <Badge className="hover:bg-primary/90 flex h-7 items-center justify-between gap-1 py-1 pr-1 pl-1.5">
-            RD-R8000-GS
-          </Badge>
-          <small className="text-nowrap">
-            RD: Rear Derailleur
-            <br />
-            R8000: Ultegra 11spd Road Groupset (R8000 Series)
-            <br />
-            GS: Medium Cage (Grand Sport)
-          </small>
-        </div>
-        <div className="flex flex-col">
-          <Badge className="hover:bg-primary/90 flex h-7 items-center justify-between gap-1 py-1 pr-1 pl-1.5">
-            RD-R8000-GS
-          </Badge>
-          <small className="text-nowrap">
-            RD: Rear Derailleur
-            <br />
-            R8000: Ultegra 11spd Road Groupset (R8000 Series)
-            <br />
-            GS: Medium Cage (Grand Sport)
-          </small>
-        </div>
         <p>
           Shimano product codes are marked on the product itself. Characters
-          before the first dash denote the category of the product. Characters
-          after the first dash denote the product's group and identifier.
-          Characters after a second dash denote additional traits of the
-          specific product.
+          before the first dash denote the category of the product. The group of
+          characters after the first dash denote the product's series and serve
+          as its main identifier. Characters after a second dash denote
+          additional traits of the product.
+        </p>
+        <div className="flex flex-col gap-3">
+          <Badge className="hover:bg-primary/90 pb-1.3 flex h-7 items-center justify-between gap-1 pr-1 pl-1">
+            RD-R8000-GS
+          </Badge>
+          <small className="text-nowrap">
+            RD: Rear Derailleur
+            <br />
+            R8000: Ultegra 11spd Road Groupset (R8000 Series)
+            <br />
+            GS: Medium Cage (Grand Sport)
+          </small>
+        </div>
+      </div>
+      <h2 className="pb-3 pl-1">Compatibility Set</h2>
+      <div className="flex flex-col gap-4">
+        <p>
+          BikePartZ filters to reveal components that are compatible with a
+          particular set of component codes,{" "}
+          <b className="font-bold text-nowrap">Selected Codes</b>.
+        </p>
+        <p className="font-extrabold">
+          Displayed codes in a Compatibility Option are not necessarily all
+          compatible with each other. They are compatible with your Selected
+          Codes.
+        </p>
+        <p>
+          Continue selecting component codes in order to filter down your{" "}
+          <b className="font-bold text-nowrap">Compatibility Option</b> group
+          and build your own compatibility set.
+        </p>
+        <p>
+          Some codes may have an active link! Some codes may also be expanded to
+          view relevant notes specific to a code in its particular{" "}
+          <b className="font-bold text-nowrap">Compatibility Option</b> group.
         </p>
       </div>
-
-      <h2 className="mb-2 pl-1">Data Source</h2>
+      <h2 className="pb-3 pl-1">Data Source</h2>
       <div className="flex flex-col gap-4">
         <p>BikePartZ current data:</p>
-        <div className="flex flex-col">
-          <h4>Shimano Compatibility</h4>
-          <h6>2024-2025 ver. 3.0</h6>
+        <div className="flex flex-col gap-0">
+          <h4>Shimano Compatibility </h4>
+          <h6 className="font-light">2024-2025 ver. 3.0</h6>
         </div>
         <p>
           Component compatibility information is sourced from Shimano's official
           products compatibility information PDFs. Shimano releases new versions
-          throughout the year.{" "}
-          <b className="font-extrabold">
-            New releases are not automatically added to BikePartZ.
-          </b>
+          throughout the year.
         </p>
+
+        <p className="font-extrabold">
+          New releases are not automatically added to BikePartZ.
+        </p>
+
         <p>
           While we strive for accuracy, edge cases exist, and errors can happen.
-          Double-check with the official manufacturer documentation for
-          certainty.
+          It is recommended to verify with manufacturer documentation.
         </p>
         <a
           className="underline"
